ecc10ced1e761f13faa51eb609c48424136ecc85
zidi nirranthea <nirranthea@gmail.com>
2019-12-28 21:57:19 +0700

    add toggle favorite

--- lib/main.dart
+++ lib/main.dart
@@ -30,13 +30,8 @@ class MyApp extends StatelessWidget {
               ],
             ),
           ),
-          Column(children: <Widget>[Icon(Icons.star, color: Colors.red[500])]),
           Column(children: <Widget>[
-            Text(
-              '41',
-              style: TextStyle(fontSize: 20.0),
-            )
-          ]),
+            FavouriteWidget()]),
         ],
       ),
     );
@@ -123,6 +118,52 @@ class MyApp extends StatelessWidget {
               color: color,
             ),
           ),
+        ),
+      ],
+    );
+  }
+}
+
+//TODO: II-1 create a stateful widget
+class FavouriteWidget extends StatefulWidget{
+  @override
+  _FavouriteWidgetState createState() => _FavouriteWidgetState();
+}
+
+class _FavouriteWidgetState extends State<FavouriteWidget>{
+  bool _isFavourited = true;
+  int _favouriteCount = 41;
+
+  void _toggleFavourite() {
+    setState((){
+      if (_isFavourited){
+        _favouriteCount -= 1;
+        _isFavourited = false;
+      } else{
+        _favouriteCount += 1;
+        _isFavourited = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_isFavourited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavourite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+            child: Container(
+              child: Text('$_favouriteCount'),
+            ),
         ),
       ],
     );
